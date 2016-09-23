fe4bea03b1fa14333d19f386ee59c76b67d64684
cvandrei <crille@indarium.de>
2016-09-23 15:27:28 +0200

    admit draft models

--- model/src/main/scala/com/ubirch/avatar/model/Device.scala
+++ model/src/main/scala/com/ubirch/avatar/model/Device.scala
@@ -7,14 +7,25 @@ import org.json4s.JValue
   * author: cvandrei
   * since: 2016-09-23
   */
-// TODO which fields are be optional?
-case class Device(deviceId: String,
-                  hwDeviceId: String,
-                  deviceName: String,
-                  deviceType: String,
-                  deviceConfig: JValue,
-                  tags: Set[String],
-                  deviceProperties: JValue,
-                  subscriptions: Set[String],
-                  created: DateTime,
-                  updated: DateTime)
+case class Device(id: String,
+                  name: Option[String],
+                  hwType: Option[String],
+                  hwId: Option[String],
+                  syncState: Option[Int], // 0 = out of sync, 1 = in sync, 100 = unknown
+                  tags: Option[Set[String]],
+                  properties: Option[JValue],
+                  subscriptions: Option[Set[String]],
+                  config: Option[JValue],
+                  avatar: Option[Avatar],
+                  created: Option[DateTime],
+                  updated: Option[DateTime],
+                  lastActive: Option[DateTime]
+                 )
+
+case class Avatar(id: String,
+                  stateDesired: Option[JValue],
+                  stateReported: Option[JValue],
+                  created: Option[DateTime],
+                  updated: Option[DateTime],
+                  lastActive: Option[DateTime]
+                 )
