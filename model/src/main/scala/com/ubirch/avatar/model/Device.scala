de4dad2bdb56e8197100c48645a25b00b526cd24
cvandrei <crille@indarium.de>
2016-09-23 17:49:25 +0200

    mark previous json model as draft; introduce working version for current prototype

--- model/src/main/scala/com/ubirch/avatar/model/Device.scala
+++ model/src/main/scala/com/ubirch/avatar/model/Device.scala
@@ -7,25 +7,52 @@ import org.json4s.JValue
   * author: cvandrei
   * since: 2016-09-23
   */
-case class Device(id: String,
-                  name: Option[String],
-                  hwType: Option[String],
-                  hwId: Option[String],
-                  syncState: Option[Int], // 0 = out of sync, 1 = in sync, 100 = unknown
-                  tags: Option[Set[String]],
-                  properties: Option[JValue],
-                  subscriptions: Option[Set[String]],
-                  config: Option[JValue],
-                  avatar: Option[Avatar],
+
+case class Device(deviceId: String,
+                  deviceType: Option[String],
+                  deviceName: Option[String],
+                  hwDeviceId: Option[String],
+                  syncState: Option[String],
+                  tags: Option[Seq[String]],
+                  deviceConfig: Option[JValue],
+                  deviceProperties: Option[JValue],
+                  subscriptions: Option[Seq[String]],
+                  avatarState: Option[AvatarState],
+                  avatarLastUpdated: Option[DateTime],
                   created: Option[DateTime],
                   updated: Option[DateTime],
-                  lastActive: Option[DateTime]
+                  deviceLastUpdated: Option[DateTime]
                  )
 
-case class Avatar(id: String,
-                  stateDesired: Option[JValue],
-                  stateReported: Option[JValue],
-                  created: Option[DateTime],
-                  updated: Option[DateTime],
-                  lastActive: Option[DateTime]
-                 )
+case class AvatarState(desired: Option[JValue],
+                       reported: Option[JValue]
+                      )
+
+/**
+  * we might use this draft in the future
+  */
+case class DeviceDraft(id: String,
+                       name: Option[String],
+                       hwType: Option[String],
+                       hwId: Option[String],
+                       syncState: Option[Int], // 0 = out of sync, 1 = in sync, 100 = unknown
+                       tags: Option[Set[String]],
+                       properties: Option[JValue],
+                       subscriptions: Option[Set[String]],
+                       config: Option[JValue],
+                       avatar: Option[AvatarDraft],
+                       created: Option[DateTime],
+                       updated: Option[DateTime],
+                       lastActive: Option[DateTime]
+                      )
+
+/**
+  * we might use this draft in the future
+  */
+case class AvatarDraft(id: String,
+                       stateDesired: Option[JValue],
+                       stateReported: Option[JValue],
+                       created: Option[DateTime],
+                       updated: Option[DateTime],
+                       lastActive: Option[DateTime]
+                      )
