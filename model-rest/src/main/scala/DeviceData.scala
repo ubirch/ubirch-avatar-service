343e93b4ff345070d4bca653f2cbf82fd5eaa099
cvandrei <crille@indarium.de>
2016-09-30 15:43:25 +0200

    add DeviceData model

--- model-rest/src/main/scala/DeviceData.scala
+++ model-rest/src/main/scala/DeviceData.scala
@@ -1,7 +1,14 @@
+import org.joda.time.DateTime
+import org.json4s.JValue
+
 /**
   * author: cvandrei
   * since: 2016-09-30
   */
-class DeviceData {
-
-}
+case class DeviceData(deviceId: String,
+                      messageId: String,
+                      deviceType: String,
+                      timestamp: DateTime,
+                      deviceTags: Seq[String],
+                      deviceMessage: JValue
+                     )
