32666bea18af8e529f12c8800950874af4efa02b
cvandrei <crille@indarium.de>
2016-10-21 14:15:28 +0200

    introduce DeviceDataStorage

---
+++ core/src/main/scala/com/ubirch/services/storage/DeviceDataStorage.scala
@@ -0,0 +1,22 @@
+package com.ubirch.services.storage
+
+import java.net.InetAddress
+
+import com.ubirch.avatar.config.Config
+
+import org.elasticsearch.client.transport.TransportClient
+import org.elasticsearch.common.transport.InetSocketTransportAddress
+
+/**
+  * author: cvandrei
+  * since: 2016-10-21
+  */
+object DeviceDataStorage extends ElasticsearchStorage {
+
+  private val address = new InetSocketTransportAddress(InetAddress.getByName(Config.esHost), Config.esPort)
+
+  override protected val esClient: TransportClient = TransportClient.builder()
+    .build()
+    .addTransportAddress(address)
+
+}
