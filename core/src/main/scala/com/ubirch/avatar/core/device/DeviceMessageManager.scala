094d352642c2791a5ba41b54d7b0405ffcfde680
cvandrei <crille@indarium.de>
2016-10-27 20:04:15 +0200

    improve scala doc

--- core/src/main/scala/com/ubirch/avatar/core/device/DeviceMessageManager.scala
+++ core/src/main/scala/com/ubirch/avatar/core/device/DeviceMessageManager.scala
@@ -9,7 +9,7 @@ import com.ubirch.util.json.{Json4sUtil, MyJsonProtocol}
 import org.elasticsearch.index.query.QueryBuilders
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionException, Future}
 
 /**
   * author: cvandrei
@@ -17,6 +17,15 @@ import scala.concurrent.Future
   */
 object DeviceMessageManager extends MyJsonProtocol {
 
+  /**
+    * Query the history of deviceMessages for a specified deviceId.
+    *
+    * @param deviceId id of the device for which we would like to get messages
+    * @param from     paging parameter: skip the first x elements
+    * @param size     paging parameter: return up to x elements
+    * @return result list; empty if no messages were found
+    * @throws ExecutionException something went wrong (e.g. no document matching our query exists yet)
+    */
   def history(deviceId: String,
               from: Int = 0,
               size: Int = Config.esDefaultPageSize
@@ -37,6 +46,12 @@ object DeviceMessageManager extends MyJsonProtocol {
 
   }
 
+  /**
+    * Store a [[DeviceMessage]].
+    *
+    * @param data device message to store
+    * @return json of what we stored
+    */
   def store(data: DeviceMessage): Future[Option[DeviceMessage]] = {
 
     Json4sUtil.any2jvalue(data) match {
