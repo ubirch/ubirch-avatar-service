cb83a2a00e447091775b7a8729c53f27dc333586
cvandrei <crille@indarium.de>
2016-11-04 14:42:50 +0100

    fixed some tests

--- server/src/test/scala/com/ubirch/avatar/backend/route/DeviceDataRawRouteSpec.scala
+++ server/src/test/scala/com/ubirch/avatar/backend/route/DeviceDataRawRouteSpec.scala
@@ -2,8 +2,8 @@ package com.ubirch.avatar.backend.route
 
 import com.ubirch.avatar.core.device.DeviceDataRawManager
 import com.ubirch.avatar.core.server.util.RouteConstants
-import com.ubirch.avatar.model.{DummyDevices, DummyDeviceDataRaw}
 import com.ubirch.avatar.model.device.DeviceDataRaw
+import com.ubirch.avatar.model.{DummyDeviceDataRaw, DummyDevices}
 import com.ubirch.avatar.test.base.{ElasticsearchSpec, RouteSpec}
 
 import akka.http.scaladsl.model.StatusCodes._
@@ -37,14 +37,14 @@ class DeviceDataRawRouteSpec extends RouteSpec
         // verify
         status shouldEqual OK
         verifyCORSHeader()
-        val storedDeviceRaw = responseAs[DeviceDataRaw]
-        storedDeviceRaw shouldEqual deviceRaw
+        val storedRaw = responseAs[DeviceDataRaw]
+        storedRaw shouldEqual deviceRaw.copy(id = storedRaw.id)
 
         Thread.sleep(1000)
 
-        val deviceRawList = Await.result(DeviceDataRawManager.history(device), 1 seconds)
-        deviceRawList.size should be(1)
-        deviceRawList.head should be(storedDeviceRaw)
+        val rawList = Await.result(DeviceDataRawManager.history(device), 1 seconds)
+        rawList.size should be(1)
+        rawList.head should be(storedRaw)
 
       }
 
@@ -54,25 +54,25 @@ class DeviceDataRawRouteSpec extends RouteSpec
 
       // prepare
       val device = DummyDevices.minimalDevice()
-      val deviceRaw1 = DummyDeviceDataRaw.data(device = device)
-      val storedDeviceRaw1 = Await.result(DeviceDataRawManager.store(deviceRaw1), 1 second).get
-      val deviceRaw2 = DummyDeviceDataRaw.data(messageId = storedDeviceRaw1.id, device = device)
+      val raw1 = DummyDeviceDataRaw.data(device = device)
+      val storedRaw1 = Await.result(DeviceDataRawManager.store(raw1), 1 second).get
+      val raw2 = DummyDeviceDataRaw.data(messageId = storedRaw1.id, device = device)
 
       // test
-      Post(RouteConstants.urlDeviceDataRaw, deviceRaw2) ~> Route.seal(routes) ~> check {
+      Post(RouteConstants.urlDeviceDataRaw, raw2) ~> Route.seal(routes) ~> check {
 
         // verify
         status shouldEqual OK
         verifyCORSHeader()
-        val storedDeviceRaw2 = responseAs[DeviceDataRaw]
-        storedDeviceRaw2 shouldEqual deviceRaw2
+        val storedRaw2 = responseAs[DeviceDataRaw]
+        storedRaw2 shouldEqual raw2.copy(id = storedRaw2.id)
 
         Thread.sleep(1000)
-        val deviceRawList = Await.result(DeviceDataRawManager.history(device), 1 seconds)
-        deviceRawList.size should be(2)
+        val rawList = Await.result(DeviceDataRawManager.history(device), 1 seconds)
+        rawList.size should be(2)
 
-        deviceRawList.head should be(storedDeviceRaw2)
-        deviceRawList(1) should be(storedDeviceRaw1)
+        rawList.head should be(storedRaw2)
+        rawList(1) should be(storedRaw1)
 
       }
 
