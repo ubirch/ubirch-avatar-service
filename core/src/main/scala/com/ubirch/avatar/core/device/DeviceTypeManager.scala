0167e6d96ba157b0d98ad03a2324c0780b8d5853
cvandrei <crille@indarium.de>
2016-11-10 16:46:45 +0100

    implemented DeviceTypeManager.create

--- core/src/main/scala/com/ubirch/avatar/core/device/DeviceTypeManager.scala
+++ core/src/main/scala/com/ubirch/avatar/core/device/DeviceTypeManager.scala
@@ -1,11 +1,14 @@
 package com.ubirch.avatar.core.device
 
+import com.typesafe.scalalogging.LazyLogging
+
 import com.ubirch.avatar.config.Config
 import com.ubirch.avatar.model.device.DeviceType
 import com.ubirch.services.storage.DeviceTypeStorage
 import com.ubirch.services.util.DeviceUtil
-import com.ubirch.util.json.JsonFormats
+import com.ubirch.util.json.{Json4sUtil, JsonFormats}
 
+import org.elasticsearch.index.query.QueryBuilders
 import org.json4s.Formats
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -15,9 +18,12 @@ import scala.concurrent.Future
   * author: cvandrei
   * since: 2016-11-09
   */
-object DeviceTypeManager {
+object DeviceTypeManager extends LazyLogging {
 
   private implicit def formats: Formats = JsonFormats.default
+
+  private val index = Config.esDeviceTypeIndex
+  private val esType = Config.esDeviceTypeType
 
   /**
     * @return all existing [[DeviceType]]s; empty if none exist
@@ -25,11 +31,25 @@ object DeviceTypeManager {
   def all(): Future[Seq[DeviceType]] = {
 
     // TODO integration tests
-    val index = Config.esDeviceTypeIndex
-    val esType = Config.esDeviceTypeType
-
     DeviceTypeStorage.getDocs(index, esType) map { res =>
       res.map(_.extract[DeviceType])
+    }
+
+  }
+
+  /**
+    * Search for a [[DeviceType]] based on it's key.
+    *
+    * @param key search criteria
+    * @return None if nothing is found, otherwise Some
+    */
+  def getByKey(key: String): Future[Option[DeviceType]] = {
+
+    // TODO integration tests
+    val query = Some(QueryBuilders.termQuery("key", key))
+
+    DeviceTypeStorage.getDocs(index, esType, query) map { res =>
+      res.map(_.extract[DeviceType]).headOption
     }
 
   }
@@ -38,13 +58,35 @@ object DeviceTypeManager {
     * Create a [[DeviceType]].
     *
     * @param deviceType deviceType to persist
-    * @return deviceType that was just created; None if it already existed
+    * @return deviceType that was just created; None if it already existed or something went wrong
     */
   def create(deviceType: DeviceType): Future[Option[DeviceType]] = {
 
     // TODO integration tests
-    // TODO implementation
-    Future(Some(deviceType))
+    val key = deviceType.key
+    Json4sUtil.any2jvalue(deviceType) match {
+
+      case Some(doc) =>
+        getByKey(key) flatMap {
+
+          case Some(dbRecord) => Future(None)
+
+          case None =>
+            DeviceTypeStorage.storeDoc(
+              docIndex = index,
+              docType = esType,
+              docIdOpt = Some(key),
+              doc = doc
+            ) map { jv =>
+              Some(jv.extract[DeviceType])
+            }
+
+        }
+
+      case None =>
+        logger.error(s"failed to convert DeviceType to JSON: deviceType=$deviceType")
+        Future(None)
+    }
 
   }
 
