b6c0b4ab714325bc3f29618b293baa96c287b88e
cvandrei <crille@indarium.de>
2016-11-10 18:54:19 +0100

    began implementing tests

--- core/src/test/scala/com/ubirch/avatar/core/device/DeviceTypeManagerSpec.scala
+++ core/src/test/scala/com/ubirch/avatar/core/device/DeviceTypeManagerSpec.scala
@@ -3,6 +3,10 @@ package com.ubirch.avatar.core.device
 import com.ubirch.avatar.test.base.ElasticsearchSpec
 import com.ubirch.util.json.MyJsonProtocol
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
 /**
   * author: cvandrei
   * since: 2016-11-10
@@ -12,89 +16,80 @@ class DeviceTypeManagerSpec extends ElasticsearchSpec
 
   feature("all()") {
 
-    // TODO implement all (ignored) test cases
-
-    ignore("index does not exist") {
-
+    scenario("index does not exist") {
+      Await.result(DeviceTypeManager.all(), 1 second) should be('isEmpty)
     }
 
     ignore("index exists; no records exist") {
-
+      // TODO implement test
     }
 
     ignore("some records exist") {
-
+      // TODO implement test
     }
 
   }
 
   feature("getByKey()") {
 
-    // TODO implement all (ignored) test cases
-
-    ignore("index does not exist") {
-
+    scenario("index does not exist") {
+      Await.result(DeviceTypeManager.getByKey("unknownDevice"), 1 second) should be(None)
     }
 
     ignore("index exists; no record matching the given key exist") {
-
+      // TODO implement test
     }
 
     ignore("record matching the given key exists") {
-
+      // TODO implement test
     }
 
   }
 
   feature("create()") {
 
-    // TODO implement all (ignored) test cases
-
     ignore("index does not exist") {
-
+      // TODO implement test
     }
 
     ignore("index exists; no record with given key exists --> create is successful") {
-
+      // TODO implement test
     }
 
     ignore("record with given key exists --> create fails") {
-
+      // TODO implement test
     }
 
   }
 
   feature("update()") {
 
-    // TODO implement all (ignored) test cases
-
     ignore("index does not exist") {
-
+      // TODO implement test
     }
 
     ignore("index exists; no record with given key exists --> update fails") {
-
+      // TODO implement test
     }
 
     ignore("record with given key exists --> update is successful") {
-
+      // TODO implement test
     }
 
   }
 
   feature("init()") {
 
-    // TODO implement all (ignored) test cases
     ignore("index does not exist --> default deviceTypes are created") {
-
+      // TODO implement test
     }
 
     ignore("index exists; no records exist --> default deviceTypes are created") {
-
+      // TODO implement test
     }
 
     ignore("records exist --> no deviceTypes are created") {
-
+      // TODO implement test
     }
 
   }
