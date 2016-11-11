2bdef9b197c6dd953c6bd3ec2b4d66665413b33f
cvandrei <crille@indarium.de>
2016-11-11 12:46:24 +0100

    payload includes a random component now

--- test-base/src/main/scala/com/ubirch/avatar/model/DummyDeviceDataRaw.scala
+++ test-base/src/main/scala/com/ubirch/avatar/model/DummyDeviceDataRaw.scala
@@ -5,11 +5,13 @@ import java.util.UUID
 import com.ubirch.avatar.model.device.{Device, DeviceDataRaw}
 import com.ubirch.crypto.hash.HashUtil
 import com.ubirch.util.uuid.UUIDUtil
+
 import org.joda.time.{DateTime, DateTimeZone}
 import org.json4s.JsonAST.JValue
 import org.json4s.native.JsonMethods._
 
 import scala.collection.mutable.ListBuffer
+import scala.util.Random
 
 /**
   * author: cvandrei
@@ -17,12 +19,14 @@ import scala.collection.mutable.ListBuffer
   */
 object DummyDeviceDataRaw {
 
+  val random = new Random()
+
   def data(messageId: UUID = UUIDUtil.uuid,
            device: Device,
            pubKey: String = "pretend-to-be-a-public-key",
            timestamp: DateTime = DateTime.now,
            hashedPubKey: String = "pretend-to-be-a-public-key",
-           payload: JValue = parse("""{"foo": 23, "bar": 42}""")
+           payload: JValue = parse(s"""{"foo": ${random.nextInt(1000000)}, "bar": ${random.nextInt(1000000)}}""")
           ): DeviceDataRaw = {
     DeviceDataRaw(id = messageId, a = device.hwDeviceId, k = pubKey, ts = timestamp, s = hashedPubKey, p = payload)
   }
