0e5d766b06dc7ccdb8be2e1836b45bd5c4a4f1b9
derMicha <dermicha@indarium.de>
2016-11-24 21:08:05 +0100

    added simple run script, added prod log conf

---
+++ runProd.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "starting avatar service"
+
+java="java"
+
+jar="./server/target/scala-2.11/server-assembly-0.3.3-SNAPSHOT.jar"
+
+params="-Dconfig.resource=application.prod.conf -Dlogback.configurationFile=logback.prod.xml"
+
+jvmparams="-Xms1g -Xmx4g -Djava.awt.headless=true -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly -XX:+DisableExplicitGC -Dfile.encoding=UTF-8"
+
+$java $params -jar $jar $jvmparams
