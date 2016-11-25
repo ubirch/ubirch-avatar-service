f2b93aa2316bf77327eff8baf78e400d28359a8b
derMicha <dermicha@indarium.de>
2016-11-25 09:01:02 +0100

    com.typesafe.scalalogging.LazyLogging -> com.typesafe.scalalogging.StrictLogging part 2

--- server/src/main/scala/com/ubirch/avatar/backend/route/DeviceTypeRoute.scala
+++ server/src/main/scala/com/ubirch/avatar/backend/route/DeviceTypeRoute.scala
@@ -4,7 +4,7 @@ import com.ubirch.avatar.util.server.RouteConstants
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.server.Route
-import com.typesafe.scalalogging.StrictLogging
+import com.typesafe.scalalogging.slf4j.StrictLogging
 import com.ubirch.avatar.backend.ResponseUtil
 import com.ubirch.avatar.core.device.DeviceTypeManager
 import com.ubirch.avatar.model.device.DeviceType
