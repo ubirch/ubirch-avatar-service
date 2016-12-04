be26249b8c4ff0f46787be6b95211b5ae903b776
Falko Zurell <falko.zurell@ubirch.com>
2016-12-04 21:30:55 +0100

    make gobuild.sh great again
    Signed-off-by: Falko Zurell <falko.zurell@ubirch.com>

--- goBuild.sh
+++ goBuild.sh
@@ -1,3 +1,40 @@
-#!/bin/sh -x
+#!/bin/bash -x
 
-docker run --user `id -u`:`id -g` --env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -v $PWD:/build --entrypoint /build/sbt ubirch/java clean compile test
+SBT_CONTAINER_VERSION="latest"
+
+
+
+function init() {
+
+  DEPENDENCY_LABEL=$GO_DEPENDENCY_LABEL_SBT_CONTAINER
+
+
+  if [ -z ${DEPENDENCY_LABEL} ]; then
+    SBT_CONTAINER_VERSION="latest"
+  else
+    SBT_CONTAINER_VERSION="v${DEPENDENCY_LABEL}"
+  fi
+
+
+}
+
+function build_software() {
+	
+  docker run --user `id -u`:`id -g` --env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY  --volume=${PWD}:/build ubirch/sbt-build:${SBT_CONTAINER_VERSION} clean compile test
+  if [ $? -ne 0 ]; then
+      echo "Docker build failed"
+      exit 1
+  fi
+}
+
+case "$1" in
+    build)
+        init
+        build_software
+        ;;
+    *)
+        echo "Usage: $0 {build|publish}"
+        exit 1
+esac
+
+exit 0
