c43915117a6ef412d514414f0e5d075b50dffc14
cvandrei <crille@indarium.de>
2017-01-10 16:42:45 +0100

    code format

--- server/src/main/scala/com/ubirch/avatar/backend/route/DeviceUpdateRoute.scala
+++ server/src/main/scala/com/ubirch/avatar/backend/route/DeviceUpdateRoute.scala
@@ -1,11 +1,13 @@
 package com.ubirch.avatar.backend.route
 
 import com.typesafe.scalalogging.slf4j.StrictLogging
+
 import com.ubirch.avatar.core.actor.MessageValidatorActor
 import com.ubirch.avatar.model.device.{DeviceDataRaw, DeviceStateUpdate}
 import com.ubirch.avatar.util.server.RouteConstants._
 import com.ubirch.util.json.MyJsonProtocol
 import com.ubirch.util.rest.akka.directives.CORSDirective
+
 import akka.actor.{ActorSystem, Props}
 import akka.http.scaladsl.server.Route
 import akka.pattern.ask
@@ -13,8 +15,10 @@ import akka.routing.RoundRobinPool
 import akka.util.Timeout
 import com.ubirch.util.http.response.ResponseUtil
 import com.ubirch.util.model.{JsonErrorResponse, JsonResponse}
+
 import de.heikoseeberger.akkahttpjson4s.Json4sSupport._
 
+import scala.concurrent.ExecutionContextExecutor
 import scala.concurrent.duration._
 import scala.language.postfixOps
 import scala.util.{Failure, Success}
@@ -29,7 +33,7 @@ trait DeviceUpdateRoute extends MyJsonProtocol
   with ResponseUtil {
 
   implicit val system = ActorSystem()
-  implicit val executionContext = system.dispatcher
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
   implicit val timeout = Timeout(15 seconds)
 
   private val validatorActor = system.actorOf(new RoundRobinPool(3).props(Props[MessageValidatorActor]), "message-validator")
@@ -42,10 +46,8 @@ trait DeviceUpdateRoute extends MyJsonProtocol
             onComplete(validatorActor ? sdm) {
               case Success(resp) =>
                 resp match {
-                  case dm: DeviceStateUpdate =>
-                    complete(dm)
-                  case jer: JsonErrorResponse =>
-                    complete(requestErrorResponse(jer))
+                  case dm: DeviceStateUpdate => complete(dm)
+                  case jer: JsonErrorResponse => complete(requestErrorResponse(jer))
                   case _ =>
                     complete(requestErrorResponse(
                       errorType = "UnknownResult",
