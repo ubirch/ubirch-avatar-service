79330c2655258195c8026d220d50cd314dfd756e
derMicha <dermicha@indarium.de>
2017-02-26 09:24:50 +0100

    now we could process simple text requests

---
+++ server/src/main/scala/com/ubirch/avatar/backend/route/DeviceUpdatePlainRoute.scala
@@ -0,0 +1,69 @@
+package com.ubirch.avatar.backend.route
+
+import akka.actor.{ActorSystem, Props}
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.pattern.ask
+import akka.routing.RoundRobinPool
+import akka.util.Timeout
+import com.typesafe.scalalogging.slf4j.StrictLogging
+import com.ubirch.avatar.config.Config
+import com.ubirch.avatar.core.actor.MessageValidatorActor
+import com.ubirch.avatar.model.device.{DeviceDataRaw, DeviceStateUpdate}
+import com.ubirch.avatar.util.actor.ActorNames
+import com.ubirch.avatar.util.server.RouteConstants.update
+import com.ubirch.util.json.{Json4sUtil, MyJsonProtocol}
+
+import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scala.util.{Failure, Success}
+
+/**
+  * Created by derMicha on 26/02/17.
+  */
+trait DeviceUpdatePlainRoute
+  extends MyJsonProtocol
+    with StrictLogging {
+
+  implicit val system = ActorSystem()
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+  implicit val timeout = Timeout(Config.actorTimeout seconds)
+
+  private val validatorActor = system.actorOf(new RoundRobinPool(Config.akkaNumberOfWorkers).props(Props[MessageValidatorActor]), ActorNames.MSG_VALIDATOR)
+
+  val route: Route = {
+    post {
+      path(update) {
+        entity(as[String]) { ddrString =>
+          Json4sUtil.string2JValue(ddrString) match {
+            case Some(ddrJson) =>
+              ddrJson.extractOpt[DeviceDataRaw] match {
+                case Some(ddr) =>
+                  onComplete(validatorActor ? ddr) {
+                    case Success(resp) =>
+                      resp match {
+                        case dm: DeviceStateUpdate =>
+                          //                      complete(dm)
+                          val dsuJson = Json4sUtil.any2jvalue(dm).get
+                          val dsuString = Json4sUtil.jvalue2String(dsuJson)
+                          complete(dsuString)
+                        case _ =>
+                          complete("NOK: DeviceStateUpdate failed")
+                      }
+                    case Failure(t) =>
+                      logger.error("update device data failed", t)
+                      complete("NOK: internal Server Error")
+                  }
+                case None =>
+                  complete("NOK: wrong json")
+              }
+
+            case None =>
+              complete("NOK: invalid json input")
+          }
+        }
+      }
+    }
+  }
+}
