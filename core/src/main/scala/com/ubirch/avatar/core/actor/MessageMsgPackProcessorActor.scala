352456f34b88fba2b5cd95cdd06ec18a443318d5
derMicha <dermicha@dermicha.de>
2017-10-21 14:10:17 +0200

    fixed response handling

--- core/src/main/scala/com/ubirch/avatar/core/actor/MessageMsgPackProcessorActor.scala
+++ core/src/main/scala/com/ubirch/avatar/core/actor/MessageMsgPackProcessorActor.scala
@@ -2,9 +2,12 @@ package com.ubirch.avatar.core.actor
 
 import java.io.ByteArrayInputStream
 
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import akka.http.scaladsl.HttpExt
+import akka.pattern.ask
 import akka.stream.Materializer
+import akka.util.Timeout
+import com.ubirch.avatar.config.Config
 import com.ubirch.avatar.core.msgpack.MsgPacker
 import com.ubirch.avatar.model.rest.MessageVersion
 import com.ubirch.avatar.model.rest.device.DeviceDataRaw
@@ -18,6 +21,8 @@ import org.msgpack.ScalaMessagePack
 import org.msgpack.`type`.ValueType
 
 import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 /**
   * Created by derMicha on 18/07/17.
@@ -30,6 +35,7 @@ class MessageMsgPackProcessorActor(implicit mongo: MongoUtil, httpClient: HttpEx
     with ActorLogging {
 
   implicit val executionContext: ExecutionContextExecutor = context.dispatcher
+  implicit val timeout = Timeout(Config.actorTimeout seconds)
 
   private val validatorActor = context.system.actorOf(Props(new MessageValidatorActor()))
   //private val validatorActor = context.system.actorSelection(ActorNames.MSG_VALIDATOR)
@@ -39,12 +45,18 @@ class MessageMsgPackProcessorActor(implicit mongo: MongoUtil, httpClient: HttpEx
     case binData: Array[Byte] =>
       val s = sender()
 
-
       try {
         val unpacker = ScalaMessagePack.messagePack.createUnpacker(new ByteArrayInputStream(binData))
         unpacker.getNextType match {
           case ValueType.ARRAY =>
-            processMsgPack(binData)
+            processMsgPack(sender, binData) match {
+              case Some(ddrs) =>
+                ddrs.foreach(validatorActor forward _)
+              case None =>
+                val hexVal = Hex.encodeHexString(binData)
+                s ! JsonErrorResponse(errorType = "Validation Error", errorMessage = s"Invalid MsgPack Input Data: $hexVal")
+            }
+          //s ! JsonResponse(message = "simple bulk processing startet")
           case ValueType.INTEGER =>
             processLegacyMsgPack(binData)
           case vt: ValueType =>
@@ -62,15 +74,15 @@ class MessageMsgPackProcessorActor(implicit mongo: MongoUtil, httpClient: HttpEx
   }
 
 
-  private def processMsgPack(binData: Array[Byte]) = {
+  private def processMsgPack(s: ActorRef, binData: Array[Byte]): Option[List[DeviceDataRaw]] = {
     val hexVal = Hex.encodeHexString(binData)
     log.info(s"got some msgPack data: $hexVal")
 
     MsgPacker.unpackTimeseries(binData) match {
       case Some(mpData) =>
         log.debug(s"msgPack data. $mpData")
-        mpData.payload.children.foreach { p =>
-          val ddr = DeviceDataRaw(
+        val ddrs = mpData.payload.children.map { p =>
+          DeviceDataRaw(
             v = MessageVersion.v001,
             a = HashUtil.sha512Base64(mpData.hwDeviceId.toLowerCase),
             s = Some(DeviceCoreUtil.createSimpleSignature(p, mpData.hwDeviceId)),
@@ -78,10 +90,10 @@ class MessageMsgPackProcessorActor(implicit mongo: MongoUtil, httpClient: HttpEx
             p = p,
             ts = mpData.created
           )
-          validatorActor forward ddr
         }
+        Some(ddrs)
       case None =>
-        sender ! JsonErrorResponse(errorType = "Validation Error", errorMessage = s"Invalid MsgPack Input Data: $hexVal")
+        None
     }
   }
 
