dbe629e17ad8dbb8341b00a84a9b08c52ab7bb0b
derMicha <dermicha@dermicha.de>
2018-02-05 23:28:01 +0100

    perf fix final

--- core/src/main/scala/com/ubirch/transformer/actor/TransformerPreprocessorActor.scala
+++ core/src/main/scala/com/ubirch/transformer/actor/TransformerPreprocessorActor.scala
@@ -10,7 +10,7 @@ import com.ubirch.avatar.model.rest.device.{DeviceDataRaw, DeviceDataRawEnvelope
 import com.ubirch.avatar.util.actor.ActorNames
 import com.ubirch.avatar.util.model.DeviceTypeUtil
 import com.ubirch.util.json.MyJsonProtocol
-import org.json4s.JValue
+import org.json4s.{JValue, MappingException}
 
 import scala.concurrent.ExecutionContextExecutor
 
@@ -41,15 +41,22 @@ class TransformerPreprocessorActor
 
         drd.v match {
 
-          case MessageVersion.`v000` =>
+          case v if Set(MessageVersion.`v000`, MessageVersion.`v001`, MessageVersion.`v002`).contains(v) =>
 
-            transformPostActor ! (dt, device, drd)
-
-          case MessageVersion.`v001` =>
-            transformPostActor ! (dt, device, drd)
-
-          case MessageVersion.`v002` =>
-            transformPostActor ! (dt, device, drd)
+            try {
+              drd.p.extract[Array[JValue]].foreach { p =>
+                log.debug(s"extracted payload: $p")
+                val newDrd = drd.copy(
+                  p = p
+                )
+                transformPostActor ! (dt, device, newDrd, drd)
+              }
+            }
+            catch {
+              case e: MappingException =>
+                log.debug(s"extracted payload: ${drd.p}")
+                transformPostActor ! (dt, device, drd)
+            }
 
           case MessageVersion.`v003` =>
             drd.p.extract[Array[JValue]].foreach { payload =>
