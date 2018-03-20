552858735116b5b19024501ef3fe5c9ec172345c
derMicha <dermicha@dermicha.de>
2018-03-20 11:46:10 +0100

    fixed ecc error message again

--- core/src/main/scala/com/ubirch/avatar/core/actor/MessageValidatorActor.scala
+++ core/src/main/scala/com/ubirch/avatar/core/actor/MessageValidatorActor.scala
@@ -82,7 +82,7 @@ class MessageValidatorActor(implicit mongo: MongoUtil, httpClient: HttpExt, mate
               case true =>
                 replayFilterActor tell((drd, dev), sender = s)
               case _ =>
-                s ! logAndCreateErrorResponse(s"invalid ecc signature: ${drd.a} / ${drd.s.getOrElse("no signature")})", "ValidationError")
+                s ! logAndCreateErrorResponse(s"ecc signature check failed: ${drd.s.getOrElse("no signature")} (hwDeviceId: ${drd.a})", "ValidationError")
             }
           }
           else
