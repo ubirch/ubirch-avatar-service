0903e1f6103bebce0f5b8e88f64145be0766ef53
derMicha <dermicha@dermicha.de>
2018-04-17 23:01:38 +0200

    removed mqtt hotfix

--- core/src/main/scala/com/ubirch/avatar/core/actor/DeviceStateUpdateActor.scala
+++ core/src/main/scala/com/ubirch/avatar/core/actor/DeviceStateUpdateActor.scala
@@ -26,7 +26,7 @@ class DeviceStateUpdateActor(deviceUuid: String)
   override def endpointUri: String = s"mqtt:" +
     s"DeviceStateUpdateActor?host=$mqttBrokerUrl&publishTopicName=$mqttDeviceOutTopic&clientId=$clientId&userName=$mqttUser&password=$mqttPassword"
 
-  //s"DeviceStateUpdateActor?host=$mqttBrokerUrl&publishTopicName=$mqttDeviceOutTopic&userName=$mqttUser&password=$mqttPassword"
+  //    s"DeviceStateUpdateActor?host=$mqttBrokerUrl&publishTopicName=$mqttDeviceOutTopic&userName=$mqttUser&password=$mqttPassword"
 }
 
 object DeviceStateUpdateActor {
