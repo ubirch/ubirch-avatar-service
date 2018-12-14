b83d1c750ad18b69708fcf8c6f56be10dc9b4001
derMicha <dermicha@ubirch.com>
2018-12-14 11:56:51 +0100

    refactored deepCheck

--- core/src/main/scala/com/ubirch/avatar/core/check/DeepCheckManager.scala
+++ core/src/main/scala/com/ubirch/avatar/core/check/DeepCheckManager.scala
@@ -4,15 +4,11 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.HttpExt
 import akka.stream.Materializer
 import com.typesafe.scalalogging.slf4j.StrictLogging
-import com.ubirch.avatar.config.Config
-import com.ubirch.avatar.core.avatar.AvatarStateManager
 import com.ubirch.keyservice.client.rest.KeyServiceClientRest
 import com.ubirch.user.client.rest.UserServiceClientRest
 import com.ubirch.util.deepCheck.model.DeepCheckResponse
 import com.ubirch.util.deepCheck.util.DeepCheckResponseUtil
-import com.ubirch.util.elasticsearch.client.binary.storage.ESSimpleStorage
 import com.ubirch.util.mongo.connection.MongoUtil
-import com.ubirch.util.redis.RedisClientUtil
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -37,15 +33,7 @@ object DeepCheckManager extends StrictLogging {
 
     (for {
 
-      // direct dependencies
-      esDeepCheck <- ESSimpleStorage.connectivityCheck(
-        docIndex = Config.esDeviceDataRawIndex,
-        docType = Config.esDeviceDataRawType
-      )
-
-      esDeepCheckWithPrefix = DeepCheckResponseUtil.addServicePrefix("avatar-service", esDeepCheck)
-      mongoConnectivity <- AvatarStateManager.connectivityCheck()
-      redisConnectivity <- RedisClientUtil.connectivityCheck("avatar-service")
+      readyCheck <- ReadyCheckManager.connectivityCheck()
 
       // other services
       keyDeepCheck <- KeyServiceClientRest.deepCheck()
@@ -55,9 +43,7 @@ object DeepCheckManager extends StrictLogging {
 
       DeepCheckResponseUtil.merge(
         Seq(
-          esDeepCheckWithPrefix,
-          //          mongoConnectivity,
-          redisConnectivity,
+          readyCheck,
           keyDeepCheck,
           userDeepCheck
         )
@@ -66,7 +52,7 @@ object DeepCheckManager extends StrictLogging {
     }).recover {
       case e =>
         logger.error("connectivityCheck", e)
-        DeepCheckResponse(false, Seq(e.getMessage))
+        DeepCheckResponse(status = false, Seq(e.getMessage))
     }
   }
 
